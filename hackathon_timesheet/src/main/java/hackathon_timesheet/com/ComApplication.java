99defae9c39f023295ad15eb9dc2c0a1914371d1
Rajat Sharma <rajatgsharma00@gmail.com>
2025-06-13 22:17:10 +0530

    Initial commit

---
+++ hackathon_timesheet/src/main/java/hackathon_timesheet/com/ComApplication.java
@@ -0,0 +1,13 @@
+package hackathon_timesheet.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComApplication.class, args);
+	}
+
+}
