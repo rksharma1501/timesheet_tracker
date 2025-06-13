2412d2e2b242ad76ac026ddc7c7563748045e013
Rajat Sharma <rajatgsharma00@gmail.com>
2025-06-13 23:02:54 +0530

    Add the Controller  in this

---
+++ hackathon_timesheet/src/main/java/hackathon_timesheet/com/controller/TimesheetController.java
@@ -0,0 +1,23 @@
+package hackathon_timesheet.com.controller;
+
+
+import hackathon_timesheet.com.entities.Timesheet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class TimesheetController {
+
+
+    @Autowired
+    private Timesheet timesheet;
+
+
+    @PostMapping
+    public Timesheet submit(){
+        return timesheet;
+    }
+}
